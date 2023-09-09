da8a4a8234edc79299dfc9de354b9b90e3e9c953
vivian wong <32468366+vivianwong905@users.noreply.github.com>
2023-09-09 15:37:22 -0400

    add map to puppies array

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import './App.css'
+import {puppyList} from './data.js'
+import { useState } from 'react'
 
 function App() {
-
+const [puppies, setPuppies] = useState(puppyList);
+console.log(puppies);
   return (
     <>
-
+<div className="App">
+      {
+        puppies.map((puppy) => {
+             return <p key={puppy.id}>{puppy.name}</p>;
+           })
+       }
+    </div>
     </>
   )
 }
